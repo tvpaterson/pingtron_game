151972978933e9d83cc495a2908222466da4497f
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-02-26 09:01:55 +0000

    Added user reviews data to seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -19,3 +19,30 @@ db.players.insertMany([
         score: 7
     },
 ])
+
+db.reviews.insertMany([
+    {
+        name: "Donald Trump",
+        score: 420,
+        rating: 5,
+        review: "Folks, I gotta tell ya: This is the best Pong game. All other Pong games? Losers."
+    },
+    {
+        name: "Joe Rogan",
+        score: 420,
+        rating: 4,
+        review: "My gameplay experience was improved thanks to Alpha Brain, DM me for promo codes."
+    },
+    {
+        name: "IGN",
+        score: 10,
+        rating: 3,
+        review: "The game really makes you feel like you are Pong."
+    },
+    {
+        name: "Big Chungus",
+        score: 69,
+        rating: 1,
+        review: "ayy lmao"
+    }
+])
