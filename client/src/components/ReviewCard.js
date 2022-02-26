0c7188c5923a5cd97c138f5a1d0209f5f097a17c
Ryan Caulfield <ryancaulfield3103@gmail.com>
2022-02-26 16:26:23 +0000

    got review list to display star ratings along with details of reviews

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,20 +1,25 @@
-const ReviewCard = ({review}) => {
+const ReviewCard = ({ review }) => {
+  // console.log(review);
+  // const handleDelete = () => {
+  //     deleteReview(review._id).then(()=>{
+  //         removeReview(review._id);
+  //     })
+  // }
+  const starRating = () => {
+    const star = "⭐️";
+    return star.repeat(review.rating);
+  };
 
-    // console.log(review);
-    // const handleDelete = () => {
-    //     deleteReview(review._id).then(()=>{
-    //         removeReview(review._id);
-    //     })
-    // }
-    return (
-        <>
-            <h1>{review.name}</h1>
-            <p>Rating: {review.rating}</p>
-            <p>{review.review}</p>
-            {/* <button onClick={handleDelete}> 🗑 </button> */}
-            <hr></hr>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>{review.name}</h1>
+      {/* <p>Rating: {review.rating}</p> */}
+      <p>{review.review}</p>
+      {starRating()}
+      {/* <button onClick={handleDelete}> 🗑 </button> */}
+      <hr></hr>
+    </>
+  );
+};
 
 export default ReviewCard;
--- client/src/components/ReviewsList.js
+++ client/src/components/ReviewsList.js
@@ -1,16 +1,11 @@
 import ReviewCard from "./ReviewCard";
 
-const ReviewsList = ({reviews}) => {
-    const reviewsList = reviews.map((review) =>{
-        return <ReviewCard review={review} key={review._id}/>
-    });
-    
-    return (
-        <>
-            {reviewsList}
-        </>
-    );
+const ReviewsList = ({ reviews }) => {
+  const reviewsList = reviews.map((review) => {
+    return <ReviewCard review={review} key={review._id} />;
+  });
 
-}
+  return <>{reviewsList}</>;
+};
 
 export default ReviewsList;
